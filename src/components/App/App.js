db64feb19d4076fa2ebed334557a17d9d5a241dd
Ryan Scharfer <rscharfer@gmail.com>
2021-11-29 09:19:14 -0800

    implementing tablet and under layout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -21,6 +21,14 @@ const App = () => {
 
 const Main = styled.main`
   padding: 64px 32px;
+
+  @media ${QUERIES.tabletAndSmaller} {
+    padding: 48px 32px
+  }
+
+  @media ${QUERIES.phoneAndSmaller} {
+    padding: 48px 16px
+  }
 `;
 
 export default App;
--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-import { WEIGHTS } from '../../constants';
+import { QUERIES, WEIGHTS } from '../../constants';
 
 import Breadcrumbs from '../Breadcrumbs';
 import Select from '../Select';
@@ -9,32 +9,43 @@ import Spacer from '../Spacer';
 import ShoeSidebar from '../ShoeSidebar';
 import ShoeGrid from '../ShoeGrid';
 
+const AppBreadCrumbs = () => (<Breadcrumbs>
+  <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
+  <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
+  <Breadcrumbs.Crumb href="/sale/shoes">
+    Shoes
+  </Breadcrumbs.Crumb>
+</Breadcrumbs>)
+
 const ShoeIndex = ({ sortId, setSortId }) => {
   return (
     <Wrapper>
       <MainColumn>
         <Header>
+        <div>
+          <MobileBreadCrumbs>
+            <AppBreadCrumbs/>
+          </MobileBreadCrumbs>
           <Title>Running</Title>
-          <Select
-            label="Sort"
-            value={sortId}
-            onChange={(ev) => setSortId(ev.target.value)}
-          >
-            <option value="newest">Newest Releases</option>
-            <option value="price">Price</option>
-          </Select>
+        </div>
+          <SelectWrapper>
+            <Select
+              label="Sort"
+              value={sortId}
+              onChange={(ev) => setSortId(ev.target.value)}
+            >
+              <option value="newest">Newest Releases</option>
+              <option value="price">Price</option>
+            </Select>
+          </SelectWrapper>
         </Header>
         <Spacer size={32} />
         <ShoeGrid />
       </MainColumn>
       <LeftColumn>
-        <Breadcrumbs>
-          <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale/shoes">
-            Shoes
-          </Breadcrumbs.Crumb>
-        </Breadcrumbs>
+        <DesktopBreadCrumbs>
+          <AppBreadCrumbs/>
+        </DesktopBreadCrumbs>
         <Spacer size={42} />
         <ShoeSidebar />
       </LeftColumn>
@@ -51,6 +62,10 @@ const Wrapper = styled.div`
 
 const LeftColumn = styled.div`
   flex-basis: 248px;
+
+  @media ${QUERIES.tabletAndSmaller} {
+    display: none;
+  }
 `;
 
 const MainColumn = styled.div`
@@ -61,11 +76,33 @@ const Header = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: baseline;
+
+  @media ${QUERIES.tabletAndSmaller} {
+    align-items: flex-end;
+  }
 `;
 
 const Title = styled.h2`
   font-size: 1.5rem;
   font-weight: ${WEIGHTS.medium};
 `;
+
+const DesktopBreadCrumbs = styled.div`
+  @media ${QUERIES.tabletAndSmaller} {
+    display: none;
+  }
+`
+
+const MobileBreadCrumbs = styled.div`
+  display: none;
+  @media ${QUERIES.tabletAndSmaller} {
+    display: revert;
+  }
+`
+const SelectWrapper = styled.div`
+ @media ${QUERIES.phoneAndSmaller} {
+   display: none;
+ }
+`
 
 export default ShoeIndex;
